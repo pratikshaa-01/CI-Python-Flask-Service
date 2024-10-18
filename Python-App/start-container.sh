cb0cab515d575a9e8a7378434adbb8049a118b21
Pratiksha <pratu0104@gmail.com>
2024-10-18 20:16:06 +0530

    Update start-container.sh

--- Python-App/start-container.sh
+++ Python-App/start-container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull pratikshaa01/python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 8000:8000 pratikshaa01/python-flask-app
